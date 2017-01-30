899a1fc8caadad4c6f674d96a57637dbb8f91bef
mkspramuditha <mkspramuditha@gmail.com>
2017-01-30 23:16:01 +0530

    create sale entity

---
+++ src/AppBundle/Entity/Sale.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sale
+ *
+ * @ORM\Table(name="sale")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SaleRepository")
+ */
+class Sale
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateTime", type="datetime")
+     */
+    private $dateTime;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="soldPrice", type="float")
+     */
+    private $soldPrice;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateTime
+     *
+     * @param \DateTime $dateTime
+     * @return Sale
+     */
+    public function setDateTime($dateTime)
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get dateTime
+     *
+     * @return \DateTime 
+     */
+    public function getDateTime()
+    {
+        return $this->dateTime;
+    }
+
+    /**
+     * Set soldPrice
+     *
+     * @param float $soldPrice
+     * @return Sale
+     */
+    public function setSoldPrice($soldPrice)
+    {
+        $this->soldPrice = $soldPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get soldPrice
+     *
+     * @return float 
+     */
+    public function getSoldPrice()
+    {
+        return $this->soldPrice;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     * @return Sale
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer 
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
