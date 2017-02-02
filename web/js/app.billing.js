d165c810533bbd6d2d2291ccac45f510d2936517
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-02-02 19:28:29 +0530

    Working on tab index

--- web/js/app.billing.js
+++ web/js/app.billing.js
@@ -9,15 +9,35 @@ function initAutocomplete() {
         $("#searchProduct").autocomplete({
             source: origin + "/admin/item/getItemsByCode",
             minLength: 1,
-            select:function (event,object) {
+            select: function (event, object) {
 
                 console.log(object.item)
                 setProductBar(object.item)
+                $("#priceSelect").focus()
             }
         });
     });
 };
 
 function setProductBar(data) {
-
+    $('.ikro-product-row .itemCode').html(data.itemCode)
+    $('.ikro-product-row .itemType').html(data.itemType)
+    $('.ikro-product-row .itemName').html(data.itemName)
+    addToSelectElement(".ikro-product-row #priceSelect", ["ABC", "ABC", "ABC", "ABC", "ABC"])
 }
+
+function addToSelectElement(element, selectValues) {
+    $(element).html("")
+    setUnitPriceCount(selectValues)
+    $.each(selectValues, function (key, value) {
+        $(element)
+            .append($("<option></option>")
+                .attr("value", key)
+                .text(value));
+    });
+}
+function setUnitPriceCount(selectValues) {
+    $('#unitPriceCaption').html("Unit Price (" + selectValues.length + ")")
+}
+
+
