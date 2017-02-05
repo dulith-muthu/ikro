52106ffc3a1ef14376cececbe736b5013a158903
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-02-06 01:59:51 +0530

    Add customer search autocomplete

--- web/js/app.billing.customer.js
+++ web/js/app.billing.customer.js
@@ -3,14 +3,39 @@ $(function () {
     $(".mdl-layout__drawer-button").on('click', function () {
         openSidebar()
     })
+    initAutocompleteCustomer()
 
 })
+function initAutocompleteCustomer() {
+    var href = window.location.href
+
+    $(function () {
+
+        var nicSearchBox = $("#customerNIC")
+        nicSearchBox.autocomplete({
+            autoFocus: true,
+            source: href + "/getItemsByCode",
+            minLength: 1,
+            select: function (event, object) {
+
+                console.log(object.item)
+
+
+            }
+        });
+        nicSearchBox.bind('focus', function () {
+            $(this).autocomplete("search");
+        });
+    });
+}
 function openSidebar() {
     //$(".mdl-layout__drawer-button").click()
     setTimeout(function () {
         isSidebar = $(".mdl-layout__drawer").hasClass("is-visible")
-        if(isSidebar){
-            focusInput("#customerName")
+        if (isSidebar) {
+            focusInput("#customerNIC")
+        } else {
+            $("#customerNIC").autocomplete("close");
         }
     }, 100)
 }
