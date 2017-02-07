14e458f2f7e05a9e133646e2224ee0bb5fa1620a
mkspramuditha <mkspramuditha@gmail.com>
2017-02-07 09:08:12 +0530

    New commit

--- src/AppBundle/Controller/BillingController.php
+++ src/AppBundle/Controller/BillingController.php
@@ -63,7 +63,6 @@ class BillingController extends BaseController
     private function generateInvoice($data){
         $data = $this->objectDeserialize($data);
         $invoice = [];
-        var_dump($data);
 
         $customer = $data->customer;
         foreach ($data->data as $row){
